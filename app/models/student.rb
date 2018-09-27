04853b0c79bcbb9230a35750bddfe4b9ff82f5e7
Daniel Loyd <dhloyd@icloud.com>
2018-09-27 18:47:01 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,18 +1,3 @@
 class Student < ActiveRecord::Base
 
-attr_accessor :name, :grade, :birthdate
-
-# def initialize(name, grade, birthdate)
-#   @name = name
-#   @grade = grade
-#   @birthdate = birthdate
-# end
-
-  def change
-      create_table :artists do |t|
-        t.string :name
-        t.string :birthdate
-      end
-  end
-
 end
